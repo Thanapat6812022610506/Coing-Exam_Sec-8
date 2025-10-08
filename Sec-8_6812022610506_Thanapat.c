0ecb75fc4865e1d51e72da29c4329594dc29b90f
Thanapat6812022610506 <s6812022610506@email.kmutnb.ac.th>
2025-10-08 15:16:25 +0700

    Add files via upload

---
+++ Sec-8_6812022610506_Thanapat.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void function () {
+    printf("*** Library Fine CAlculator***\n")
+}
+int main(){ 
+int Book [2]; 
+printf("Enter number of late book(1-10) : ") ;
+scanf("%d" , &Book) ;
+int Day ;
+printf("Enter late day for book : ") ;
+scanf("%d" , &Day) ;
+int Type ;
+printf("Enter Book Tyoe (F,A,R) :") ;
+scanf("%d", &Type)
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
